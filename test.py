f4364cdd2095a6367d73ed55fa6ef2f644098b57
Kyle-Gifford <cakyle@gmail.com>
2022-10-17 22:25:01 -0700

    add gitignore

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # python version 3.9.7
-# pip install -Iv flask==2.2.2
-# pip install -Iv flask-mysqldb==1.0.1
+# pip install flask==2.2.2
+# pip install flask-mysqldb==1.0.1
 
 from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
