bb28d48ffe2c15c246ee9bf37dc59daaca858d58
Kyle-Gifford <cakyle@gmail.com>
2022-10-18 08:54:49 -0700

    Sign up and login/logout working

--- test.py
+++ test.py
@@ -4,8 +4,12 @@
 
 
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
 from credentials import port as PORT
+
+user_data = {'emails': ['jpi@usr.edu'],
+                'active_user': None,
+                'jpi@usr.edu': {'name': 'joe', 'password': 'jip', 'email': 'jpi@usr.edu'}}
 
 
 app = Flask(__name__)
@@ -13,6 +17,53 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template('index.j2')
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template('signup.j2')
+    else:
+        email = request.form["email"]
+        password = request.form["password"]
+        name = request.form["name"]
+        if email not in user_data["emails"]:
+            user_data["emails"].append(email)
+            new_user = {"password": password, "email": email, "name": name}
+            new_user['funds'] = 0
+            user_data[str(email)] = new_user
+            user_data['active_user'] = email
+        print(user_data)
+        return redirect("/home")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template('login.j2')
+    else:
+        print('posting')
+        email = request.form["email"]
+        password = request.form["password"]
+        print(user_data)
+        print(email, user_data["emails"], password, user_data[email]['password'])
+        if email in user_data["emails"] and password == user_data[email]['password']:
+            print('login successful')
+            user_data['active_user'] = email
+            print(user_data)
+            return redirect("/home")
+        else:
+            print('no')
+            return render_template('login.j2')
+
+@app.route('/home')
+def home():
+    return render_template('user_cp.j2', user_data=user_data)
+
+@app.route('/logout')
+def logout():
+    user_data['active_user'] = None
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(port=PORT, debug=True)
